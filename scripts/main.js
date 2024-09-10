b99ccf1af4c2d48cd84c780c5cc94f2a509087a7
王浩 <13700680+wangh_home@user.noreply.gitee.com>
2024-09-10 23:15:24 +0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,19 @@
+let isClicked = false;
+
+let currentImage = 1; // 初始显示第一张图片
+
 document.getElementById("woqunima").addEventListener("click", () => {
-    alert("别戳我，我怕疼。");
-  });
+    const img = document.getElementById("woqunima");
+    
+    // 根据当前显示的图片切换到另一张图片
+    if (currentImage === 1) {
+        img.src = "image/xuewaiyu.jpg"; // 切换到第二张图片
+        currentImage = 2;
+    } else {
+        img.src = "image/firefox-icon.png"; // 切换到第一张图片
+        currentImage = 1;
+    }
+});
 myHeading=document.getElementById("h1");
 function setUserName() {
 let myName = prompt("请输入你的名字。");
@@ -8,7 +21,7 @@ if (!myName) {
     setUserName();
 } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    myHeading.textContent = "你好，" + myName;
 }
 }
 document.getElementById("qiehuan").addEventListener("click", () => {
@@ -18,5 +31,5 @@ if (!localStorage.getItem("name")) {
     setUserName();
   } else {
     let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+    myHeading.textContent = "你好，" + storedName;
   }
